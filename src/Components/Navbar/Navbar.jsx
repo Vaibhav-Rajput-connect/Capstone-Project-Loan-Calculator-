3078a54ea41736bc00e44a0565172154f98b761e
Vaibhav Rajput <contact.vaibhavrajput.official@gmail.com>
2025-05-08 00:11:02 +0530

    update nav bar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import './Navbar.css'
+import React from 'react'
+const Navbar = () => {
+
+
+  return (
+    <nav >
+        <h1>HomeHarbor</h1>
+      <ul>
+        <li>
+          Home
+        </li>
+        <li>Program
+        </li>
+        <li>About Us
+        </li>
+        <li>Testimonials
+        </li>
+        <li>Contact Us
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
