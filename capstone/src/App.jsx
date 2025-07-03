b41aedad411ba0e9e7615244bf4d231e54f2eb1a
Vaibhav Rajput <contact.vaibhavrajput.official@gmail.com>
2025-07-03 20:39:47 +0530

    made header

--- capstone/src/App.jsx
+++ capstone/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">Tailwind is working!</h1>
+    <div>
+      <Header/>
     </div>
   );
 }
