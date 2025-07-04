93c5a46ea86fa87cced51bc6ee00d53785314a6d
Vaibhav Rajput <contact.vaibhavrajput.official@gmail.com>
2025-07-04 11:37:09 +0530

    changes

--- capstone/src/App.jsx
+++ capstone/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
+import Mortgage from './components/Mortgage';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <About/>
       <Projects/>
+      <Mortgage/>
 
     </div>
   );
